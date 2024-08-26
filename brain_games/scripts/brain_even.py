31da5af809364309f3548600e4315b63626926b3
ybny0nsr <oleg.tikhonoff@gmail.com>
2024-08-26 20:35:02 +0300

    fix brain_games/scripts/brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -8,25 +8,30 @@ import brain_games.cli as cli
 
 
 def main():
-    user_name = cli.welcome_user()
+    user_name = cli.welcome_user()  # приветствие, получение имени
     attempts = 3  # Кол-во попыток
     min_number, max_number = 1, 100  # диапазон номеров
-    parity = {True: 'yes', False: 'no'}
+    parity = {True: 'yes', False: 'no'}  # четность и варианты ответов
+
     print('Answer "yes" if the number is even, otherwise answer "no"')
+
     for attempt in range(attempts):
         number = random.randint(min_number, max_number)  # целое в диапазоне
+
+        print(f'Question: {number}')  # задаем вопрос
+
         is_even = number % 2 == 0  # True/False если четное/нечетное
-        print(f'Question: {number}')
-        answer = prompt.string('Your answer: ').lower()  # ответ - в прописные
-        correct_answer = parity[is_even]  # правильный ответ
-        if answer == parity[is_even]:    # удачная попытка
-            print(Fore.GREEN, 'Correct!', Fore.RESET)
+        correct_answer = parity[is_even]  # верный ответ (yes/no)
+        answer = prompt.string('Your answer: ').lower()  # ответ пользователя
+
+        if answer == correct_answer:    # удачная попытка
+            print(Fore.GREEN, 'Correct!', Fore.RESET, sep='')
         else:                           # неудачная попытка
             print(Fore.RED, f"'{answer}' is wrong answer ;(. "
                             f"Correct answer was '{correct_answer}'.\n"
-                            f"Let's try again, {user_name}", Fore.RESET)
-            break  # завершение программы
-    else:  # если все попытки - удачные
+                            f"Let's try again, {user_name}", Fore.RESET, sep='')
+            break  # завершение программы в случае неверного ответа
+    else:  # если все ответы были верные, выводим поздравление
         print(Fore.GREEN, f'Congratulations, {user_name}!', Fore.RESET)
 
 
