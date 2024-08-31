ab144fce7277dfadd8e5a50ed1ecf93d96e0ea5d
ybny0nsr <oleg.tikhonoff@gmail.com>
2024-08-31 13:16:08 +0300

    update files in directories scripts/ and games/

---
+++ brain_games/games/brain_calc.py
@@ -0,0 +1,43 @@
+import random
+import prompt
+from colorama import Fore
+import brain_games.cli as cli
+
+
+def main():
+    user_name = cli.welcome_user()
+    attempts = 3  # Кол-во попыток
+    min_nr, max_nr = 1, 100  # диапазон аргументов
+    operations = [['*', lambda a, b: a * b],  # значки операций и
+                  ['+', lambda a, b: a + b],  # собственно функции,
+                  ['-', lambda a, b: a - b]]  # реализующие операцию
+    sign, function = 0, 1  # индексы для значка и для функции операций
+
+    print('What is the result of the expression?')
+
+    for attempt in range(attempts):
+        nr_one = random.randint(min_nr, max_nr)  # генерация 1го аргумента
+        nr_two = random.randint(min_nr, max_nr)  # генерация 2го аргумента
+        if nr_two > nr_one:  # избегание отрицательных значений при разности
+            nr_one, nr_two = nr_two, nr_one
+
+        operation = random.choice(operations)  # случайный выбор операции
+        correct_answer = operation[function](nr_one,
+                                             nr_two)  # результат операции
+        print(f'Question: {nr_one} {operation[sign]} {nr_two}')
+        answer = prompt.integer('Your answer: ')
+
+        if answer == correct_answer:  # верный ответ
+            print(Fore.GREEN, 'Correct!', Fore.RESET, sep='')
+        else:  # неверный ответ
+            print(Fore.RED, f"'{answer}' is wrong answer ;(. "
+                            f"Correct answer was '{correct_answer}'.\n"
+                            f"Let's try again, {user_name}", Fore.RESET,
+                  sep='')
+            break  # завершение программы при неверном ответе
+    else:  # если все ответы верные, выводим поздравление
+        print(Fore.GREEN, f'Congratulations, {user_name}!', Fore.RESET, sep='')
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 
-from brain_games.games.brain_even import main as brain_even
+from brain_games.games.brain_gcd import main as brain_gcd
 
 
 def main():
-    brain_even()
+    brain_gcd()
 
 
 if __name__ == '__main__':
