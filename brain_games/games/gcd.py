5662c6379a638f4513952869e74c6f4a8135cb30
ybny0nsr <oleg.tikhonoff@gmail.com>
2024-09-08 21:07:01 +0300

    and one more...

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,30 +1,64 @@
+import random
 import prompt
-from colorama import Fore
-
-import brain_games.cli as cli
-import brain_games.common.settings as settings
-import brain_games.common.functions as f
 
 
-def main():
-    user_name = cli.welcome_user()  # Приветствие, запрос имени
+from brain_games.common.game_template import run_game
 
-    print(settings.TASK_GCD)  # сообщаем условия игры
 
-    for attempt in range(settings.ATTEMPTS):
-        # генерация вопроса и правильного ответа для этого вопроса
-        question, correct_answer = f.question_gcd(settings.MAX_NUMBER,
-                                                  settings.MAX_LCD)
-        print(question)  # выводим вопрос для текущей попытки
+def random_pair_gcd(max_number: int, max_lcd: int) -> tuple:
+    '''Возвращает пару случайных целых чисел в диапазоне 1...max_nr,
+    имеющих по крайней мере 1 общий целый делитель в диапазоне 1...max_lcd'''
+    if max_lcd >= max_number or max_lcd <= 1:
+        raise Exception('max_lcd have to be positive and less than max_number')
+    lcd = random.randint(1, max_lcd)  # least common divisor
+    nr_one = nr_two = random.randrange(lcd, max_number + 1, lcd)
+    while nr_one == nr_two:  # исключаем совпадение сгенерированных чисел
+        nr_two = random.randrange(lcd, max_number + 1, lcd)
+    return nr_one, nr_two
 
-        answer = prompt.integer('Your answer: ')
 
-        check_result = f.feedback(answer, correct_answer, user_name)
-        if not check_result:  # ответ неверный, завершаем игру
-            break
-    else:  # если все попытки удачные, выводим поздравление
-        print(Fore.GREEN, f'Congratulations, {user_name}!', Fore.RESET, sep='')
+def find_divisors(number: int) -> set:
+    '''Возвращает множество делителей целого числа в диапазоне 1...число
+       или {1}, если аргумент равен 0'''
+    divisors = {divisor for divisor in range(1, number + 1) if
+                number % divisor == 0} if number != 0 else {1}
+    return divisors
+
+
+def find_gcd(nr_one: int, nr_two: int) -> int:
+    '''Возвращает максимальный общий делитель для двух целых аргументов'''
+    divisors_one = find_divisors(nr_one)  # все делители для 1го аргумента
+    divisors_two = find_divisors(nr_two)  # все делители для 2го аргумента
+    common_divisors = divisors_one & divisors_two  # только общие делители
+    return max(common_divisors)  # наибольший из общих делителей
+
+
+def question_gcd(max_number: int, max_lcd: int) -> tuple:
+    '''Возвращает строку-вопрос и правильный ответ: наибольший общий делитель
+    для двух целых в диапазоне 1...max_number'''
+    nr_one, nr_two = random_pair_gcd(max_number, max_lcd)  # , max_lcd)
+    correct_answer = find_gcd(nr_one, nr_two)  # верный ответ (GCD для N1 и N2)
+    question = f'Question: {nr_one} {nr_two}'  # формулировка вопроса
+    return question, correct_answer
+
+
+def answer_request() -> int:
+    ''' Получение ответа пользователя на вопрос попытки'''
+    return prompt.integer('Your answer: ')
+
+
+def gcd():
+    # Параметры игры
+    max_number = 100  # Максимальное число для игры
+    attempts = 3  # Кол-во попыток
+    max_lcd = 9  # верхний предел для генерации случайного наименьшего общего
+    # делителя. Вспомогательный параметр для расчета GCD, рекомендуется 9...11
+    task = 'Find the greatest common divisor of given numbers.'
+
+    # передаем в шаблон параметры игры
+    run_game(task, attempts, answer_request, question_gcd,
+             (max_number, max_lcd))
 
 
 if __name__ == '__main__':
-    main()
+    gcd()
